29a9c60c5a47d2c06c4aaef31a8568d9c838850d
Philipp Schmitt <philipp@schmitt.co>
2015-04-15 11:03:31 +0200

    Add pyvmomi dependency

--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@ from distutils.core import setup
 
 setup(
     name='pyvmomi-lib',
-    version='1.0',
+    version='1.1',
     description='Retrieve the credentials for connecting to various ICT equipement',
     author='Philipp Schmitt',
     author_email='philipp.schmitt@post.lu',
     url='https://git.3s.dt.ept.lu/ict-infra/pyvmomi-lib',
     packages=['pyvmomilib'],
+    install_requires=['pyvmomi']
 )
