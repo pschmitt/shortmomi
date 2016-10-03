2bf43c4eefc91977e484eaee4672c1c062d7fcdb
Philipp Schmitt <philipp@schmitt.co>
2016-10-03 14:58:49 +0200

    Modernize setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
-from distutils.core import setup
+from setuptools import find_packages, setup
+
 
 setup(
     name='shortmomi',
     version='1.6.1',
+    license='GPL3',
     description='Bundle of useful functions and shortcuts for pyVmomi',
     author='Philipp Schmitt',
     author_email='philipp@schmitt.co',
     url='https://github.com/pschmitt/shortmomi',
-    packages=['shortmomi'],
+    packages=find_packages(),
     install_requires=['pyvmomi']
 )
