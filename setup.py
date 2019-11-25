3a4e39e5e3472a2445b144dfcfd9f6dca7ffdd0f
Philipp Schmitt <philipp@schmitt.co>
2019-11-25 13:40:59 +0100

    Version 1.8.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="shortmomi",
-    version="1.7.1",
+    version="1.8.0",
     license="GPL3",
     description="Bundle of useful functions and shortcuts for pyVmomi",
     author="Philipp Schmitt",
